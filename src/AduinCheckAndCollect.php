84f99934adfcd044b6235ce405dc6840018b5839
mehrpohl <mehrpohl@adu-inkasso.de>
2025-09-08 15:05:14 +0200

    2.6.13

--- src/AduinCheckAndCollect.php
+++ src/AduinCheckAndCollect.php
@@ -26,7 +26,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Contracts\Service\Attribute\Required;
 
-define('CHECKANDCOLLECTVERSION', '2.6.11');
+define('CHECKANDCOLLECTVERSION', '2.6.13');
 define('CHECKANDCOLLECTSALT', 'hui3h9T%$T54t$&%)="$&v56');
 
 /**
