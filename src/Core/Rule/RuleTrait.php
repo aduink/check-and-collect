aad9a075a787b74cf43f4b8cb030979f64c5c2c1
mehrpohl <mehrpohl@adu-inkasso.de>
2026-03-09 15:14:23 +0100

    Session kann bei der Prüfung leer sein

--- src/Core/Rule/RuleTrait.php
+++ src/Core/Rule/RuleTrait.php
@@ -270,12 +270,12 @@ trait RuleTrait
         }
     }
     private function blockCheck(): void{
-        $this->session->set(AduConfig::TEMP_BLOCK_CHECK, true);
+        $this->session?->set(AduConfig::TEMP_BLOCK_CHECK, true);
     }
     private function isBlocked(): bool{
-        return $this->session->get(AduConfig::TEMP_BLOCK_CHECK, false);
+        return $this->session?->get(AduConfig::TEMP_BLOCK_CHECK, false) ?? false;
     }
     private function liftBlockCheck(): void{
-        $this->session->set(AduConfig::TEMP_BLOCK_CHECK, false);
+        $this->session?->set(AduConfig::TEMP_BLOCK_CHECK, false);
     }
 }
